e347b9c7f77a03943ccdd59b018c5a508917ffbf
yli <ying.li@magic-point.com>
2018-11-26 17:57:40 +0800

    inital project

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-unused-vars": ["error", { "varsIgnorePattern": "regeneratorRuntime" }]
+    },
+    "globals": {
+      "App": true,
+      "Page": true,
+      "wx": true,
+      "getApp": true
+    }
+};
